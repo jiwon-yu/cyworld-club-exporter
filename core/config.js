12822e449cf17466fc823b1aba089d49163fcb49
jiwon-yu <60417276+jiwon-yu@users.noreply.github.com>
2020-01-29 12:59:19 +0900

    Update config.js

--- core/config.js
+++ core/config.js
@@ -2,11 +2,11 @@
  * Created by ecst_000 on 2016-01-10.
  */
 module.exports = {
-    userId: '아이디',
-    password: '비밀번호',
+    userId: 'wldnjs0747',
+    password: 'j#1011604',
     rsaPublic: 'DD303A4EB455BA81F12DFA168FBB044C99B412CF8EA149709E81A3362B6F3136D577121276CA0CB60D49F958F3FDBA66B6D6CD3FBE0789A237A2DDB42499613D77F74FE8E1DE505B8F768DBD7881759F94EFB5090AC724805759A5516702D35CDAEC7708621A0D39488CACD872BB7AD26F6F5C76E0092FC5F3377A2D2404E48F',
     rsaModulus: '10001',
-    clubId: '싸이월드 클럽번호',
+    clubId: '50713502',
     requestHeaders: {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Upgrade-Insecure-Requests': 1,
